add8f95efcffd504ac06bbeb4d59cf7f1cf9fc51
Michael Shoemake <mshoemake@wgen.net>
2013-12-25 22:31:44 -0500

    Trying to figure out single page app stuff.  This is try #1.

--- prototype/js/app-controllers.js
+++ prototype/js/app-controllers.js
@@ -1,40 +1,32 @@
 
-var cookbooks = angular.module('cookbooks', []);
+var cookbooks = angular.module('cookbooks', ['ngRoute', 'ngSanitize']);
  
-// Controllers
-//cookbooks.controller('CookbookListController', ['cookbookService', function (cookbookService)] {
-cookbooks.controller('CookbookListController', function ($scope) {
-  $scope.cookbookList = [
-    {'name': '30 Minute Meals For Dummies'}, 
-    {'name': 'allrecipes.com'}, 
-    {'name': 'Betty Crocker'}, 
-    {'name': 'Chris Carmichael\'s Fitness Cookbook'}, 
-    {'name': 'Cooking Light Magazine'}, 
-    {'name': 'Gluten Free and Easy'}, 
-    {'name': 'Gran'}, 
-    {'name': 'Grandma Grace'}, 
-    {'name': 'Grandma Hilda'}, 
-    {'name': 'Kraft Shredded Cheese Packet'}, 
-    {'name': 'Lazy Day Cookin'}, 
-    {'name': 'Mamaw Shoemake'}, 
-    {'name': 'Personal Cookbook'}, 
-    {'name': 'Pillsbury Annual Recipes 2010'}, 
-    {'name': 'The Gluten Free Bible'}, 
-    {'name': 'The Great Potato Cookbook'}, 
-    {'name': 'www.stephanieodea.com'}, 
-    {'name': 'www.tasteofhome.com'} 
-  ];
-/*
-    $scope.cookbookList = cookbookService.getAll();
-	this.list = cookbookService.getAll();  
-    window.alert('Cookbooks: ' + this.list.size());
-*/
-});
+var urlPrefix = 'file://localhost/Users/mshoemake/IdeaProjects/HomeMgr/prototype/';
+
+// Routes
+
+cookbooks.config(function($routeProvider) {
+	$routeProvider.when('/adminCookbooks', {templateUrl: urlPrefix + 'admin-cookbooks.html', controller: 'CookbookListController'});
+	$routeProvider.when('/adminMeals', {templateUrl: urlPrefix + 'admin-meals.html', controller: 'MealListController'});
+	$routeProvider.otherwise({redirectTo: '/adminCookbooks'});
 
 /*
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl : urlPrefix + 'admin-cookbooks.html',
+			controller  : CookbookListController
+		})
+		.when('/adminCookbooks', {
+			templateUrl : urlPrefix + 'admin-cookbooks.html',
+			controller  : CookbookListController
+		})
+*/		
+});
+
 // Services
-cookbooks.factory('cookbookService', function() {
-  var ListService = {};
+cookbooks.service('cookbookService', function() {
   var cookbookList = [
     {'name': '30 Minute Meals For Dummies'}, 
     {'name': 'allrecipes.com'}, 
@@ -48,6 +40,7 @@ cookbooks.factory('cookbookService', function() {
     {'name': 'Kraft Shredded Cheese Packet'}, 
     {'name': 'Lazy Day Cookin'}, 
     {'name': 'Mamaw Shoemake'}, 
+    {'name': 'Mike\'s Test Cookbook'}, 
     {'name': 'Personal Cookbook'}, 
     {'name': 'Pillsbury Annual Recipes 2010'}, 
     {'name': 'The Gluten Free Bible'}, 
@@ -55,21 +48,21 @@ cookbooks.factory('cookbookService', function() {
     {'name': 'www.stephanieodea.com'}, 
     {'name': 'www.tasteofhome.com'} 
   ];
-  cookbookService.getAll = function(index) { return cookbookList; }
-  cookbookService.getItem = function(index) { return cookbookList[index]; }
-  cookbookService.addItem = function(item) { cookbookList.push(item); }
-  cookbookService.removeItem = function(item) { cookbookList.splice(cookbookList.indexOf(item), 1) }
-  cookbookService.size = function() { return cookbookList.length; }
-
-  return cookbookService;
+  this.getAll = function() { return cookbookList; }
+  this.getItem = function(index) { return cookbookList[index]; }
+  this.addItem = function(item) { cookbookList.push(item); }
+  this.removeItem = function(item) { cookbookList.splice(cookbookList.indexOf(item), 1) }
+  this.size = function() { return cookbookList.length; }
 });
-*/
-/*
-function Ctrl1($scope, ListService) {
-  //Can add/remove/get items from shared list
-}
 
-function Ctrl2($scope, ListService) {
-  //Can add/remove/get items from shared list
-}
-*/
+// Controllers
+//AppController.$inject = ['$scope', '$route']
+//cookbooks.controller('AppController', function ($scope, $route) {
+//	$scope.$route = $route;
+//} 
+
+//cookbooks.controller('CookbookListController', ['$scope', 'cookbookService', function ($scope, cookbookService) {
+cookbooks.controller('CookbookListController', function ($scope, cookbookService) {
+	$scope.cookbookList = cookbookService.getAll();
+});
+
